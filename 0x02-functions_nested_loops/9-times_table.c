dc6a87e2ad459540e4de007f194ab7f3b7978881
Kori254 <korilewis958@gmail.com>
2023-02-21 14:38:56 -0800

    timr table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
-
 /**
  * times_table - prints the 9 times table, starting with 0
  */
+
 void times_table(void)
 {
 	int i, j, k;
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
@@ -16,6 +17,7 @@ void times_table(void)
 			{
 				_putchar(k + '0');
 			}
+
 			if (k < 10 && j != 0)
 			{
 				_putchar(',');
@@ -33,4 +35,3 @@ void times_table(void)
 		_putchar('\n');
 	}
 }
-
